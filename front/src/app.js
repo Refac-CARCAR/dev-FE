166ac1dec619c8d5c9517ccc6caa70f7989bceb4
minseong <alstjd055@gmail.com>
2023-05-02 07:37:43 +0900

    추가: "app.js 에러발생 원인주석"
    npm start 할 시 포트 중복 오류가 계속 뜨는데
    index.js와 app.js의 포트를 다르게 설정할 시 오류가 안뜸
    왜 인지는 몰라서 일단 주석만 달고 코드는 그대로 두었습니다.

--- front/src/app.js
+++ front/src/app.js
@@ -10,6 +10,8 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(viewsRouter);
 
+// index.js의 port와 중복되면  충돌이 발생해서 npm start가 안 되는데 왜지?
+// Error: listen EADDRINUSE: address already in use :::3000
 app.listen(3000, () => {
   console.log('http://localhost:3000')
 })
